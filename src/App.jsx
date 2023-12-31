0c708c7e6edbd9b9250eb1c4ea3e8729807326be
maryanabdiosman <abdimaryan41@gmail.com>
2023-12-31 13:42:47 +0100

    this Assigment of React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  {Routes , Route} from 'react-router-dom'
+import Assigment from './pages/Assigment'
+function App() {
+  return (
+    <div>
+      <Assigment />
+    </div>
+  )
+}
+
+export default App
